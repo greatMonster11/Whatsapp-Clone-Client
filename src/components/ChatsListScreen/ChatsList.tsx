2b755ded359ff7c6dcc2946ff130e51cd9094e32
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-06 00:23:25 +0700

    request dump data from running server

--- src/components/ChatsListScreen/ChatsList.tsx
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { chats } from '../../db';
+import React, { useState, useMemo } from 'react';
 import moment from 'moment';
 import { List, ListItem } from '@material-ui/core';
 import styled from 'styled-components';
@@ -56,27 +55,37 @@ const MessageDate = styled.div`
   font-size: 13px;
 `;
 
-const ChatsList: React.FC = () => (
-  <Container>
-    <StyledList>
-      {chats.map(chat => (
-        <StyledListItem key={chat.id} button>
-          <ChatPicture src={chat.picture} alt="Profile" />
-          <ChatInfo>
-            <ChatName>{chat.name}</ChatName>
-            {chat.lastMessage && (
-              <React.Fragment>
-                <MessageContent>{chat.lastMessage.content}</MessageContent>
-                <MessageDate>
-                  {moment(chat.lastMessage.createdAt).format('HH:mm')}
-                </MessageDate>
-              </React.Fragment>
-            )}
-          </ChatInfo>
-        </StyledListItem>
-      ))}
-    </StyledList>
-  </Container>
-);
+const ChatsList: React.FC = () => {
+  const [chats, setChats] = useState<any[]>([]);
+
+  useMemo(async () => {
+    const body = await fetch(`${process.env.REACT_APP_SERVER_URL}/chats`);
+    const chats = await body.json();
+    setChats(chats);
+  }, []);
+
+  return (
+    <Container>
+      <StyledList>
+        {chats.map(chat => (
+          <StyledListItem key={chat.id} button>
+            <ChatPicture src={chat.picture} alt="Profile" />
+            <ChatInfo>
+              <ChatName>{chat.name}</ChatName>
+              {chat.lastMessage && (
+                <React.Fragment>
+                  <MessageContent>{chat.lastMessage.content}</MessageContent>
+                  <MessageDate>
+                    {moment(chat.lastMessage.createdAt).format('HH:mm')}
+                  </MessageDate>
+                </React.Fragment>
+              )}
+            </ChatInfo>
+          </StyledListItem>
+        ))}
+      </StyledList>
+    </Container>
+  );
+};
 
 export default ChatsList;
