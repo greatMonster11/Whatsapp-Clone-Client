ee630d4ffa199009a75002c0880fd208821fb8f0
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-05 17:51:47 +0700

    use prettier

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,18 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to relad.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
