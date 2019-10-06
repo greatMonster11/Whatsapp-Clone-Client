09d4e6dfbcca9dc832ba108f5874939bf902c0cc
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-06 23:30:40 +0700

    mock Apollo requests in tests v2

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
 import {
   cleanup,
   render,
@@ -7,8 +8,9 @@ import {
   wait,
   waitForDomChange,
 } from '@testing-library/react';
-import ChatsList from './ChatsList';
 import { createBrowserHistory } from 'history';
+import { mockApolloClient } from '../../test-helpers';
+import ChatsList, { getChatsQuery } from './ChatsList';
 
 describe('ChatsList', () => {
   afterEach(() => {
@@ -21,35 +23,19 @@ describe('ChatsList', () => {
   });
 
   it('renders fetched chats data', async () => {
-    fetchMock.mockResponseOnce(
-      JSON.stringify({
-        data: {
-          chats: [
-            {
-              id: 1,
-              name: 'Foo Bar',
-              picture: 'https://localhost:4000/picture.jpg',
-              lastMessage: {
-                id: 1,
-                content: 'Hello',
-                createdAt: new Date('1 Jan 2019 GMT'),
-              },
-            },
-          ],
-        },
-      })
-    );
-
-    it('should navigate to the target chat room on chat item click', async () => {
-      fetchMock.mockResponseOnce(
-        JSON.stringify({
+    const client = mockApolloClient([
+      {
+        request: { query: getChatsQuery },
+        result: {
           data: {
             chats: [
               {
+                __typename: 'Chat',
                 id: 1,
                 name: 'Foo Bar',
                 picture: 'https://localhost:4000/picture.jpg',
                 lastMessage: {
+                  __typename: 'Message',
                   id: 1,
                   content: 'Hello',
                   createdAt: new Date('1 Jan 2019 GMT'),
@@ -57,14 +43,55 @@ describe('ChatsList', () => {
               },
             ],
           },
-        })
+        },
+      },
+    ]);
+
+    const history = createBrowserHistory();
+
+    {
+      const { container, getByTestId } = render(
+        <ApolloProvider client={client}>
+          <ChatsList history={history} />
+        </ApolloProvider>
       );
+      await waitForDomChange({ container });
+
+      describe('ChatsList', () => {});
+    }
+
+    it('should navigate to the target chat room on chat item click', async () => {
+      const client = mockApolloClient([
+        {
+          request: { query: getChatsQuery },
+          result: {
+            data: {
+              chats: [
+                {
+                  __typename: 'Chat',
+                  id: 1,
+                  name: 'Foo Bar',
+                  picture: 'https://localhost:4000/picture.jpg',
+                  lastMessage: {
+                    __typename: 'Message',
+                    id: 1,
+                    content: 'Hello',
+                    createdAt: new Date('1 Jan 2019 GMT'),
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ]);
 
       const history = createBrowserHistory();
 
       {
         const { container, getByTestId } = render(
-          <ChatsList history={history} />
+          <ApolloProvider client={client}>
+            <ChatsList history={history} />
+          </ApolloProvider>
         );
 
         await waitForDomChange({ container });
