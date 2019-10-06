257851d1673dc64bbd9eec7f840091924a3dbba8
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-06 22:43:47 +0700

    test ChatRoomScreen child components

--- src/components/ChatRoomScreen/MessageInput.tsx
+++ src/components/ChatRoomScreen/MessageInput.tsx
@@ -69,13 +69,18 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage }) => {
   return (
     <Container>
       <ActualInput
+        data-testid="message-input"
         type="text"
         placeholder="Type a message"
         value={message}
         onChange={onChange}
         onKeyPress={onKeyPress}
       />
-      <SendButton variant="contained" color="primary" onClick={submitMessage}>
+      <SendButton
+        data-testid="send-button"
+        variant="contained"
+        color="primary"
+        onClick={submitMessage}>
         <SendIcon />
       </SendButton>
     </Container>
