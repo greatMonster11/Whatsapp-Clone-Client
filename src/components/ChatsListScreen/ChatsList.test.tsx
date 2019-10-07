42f78e3f2e1b05cee4488320f46040cdb1d1b4ee
Phuoc Thanh <39474278+greatMonster11@users.noreply.github.com>
2019-10-07 16:50:32 +0700

    Send message with graph ql mutations (#4)
    * add GraphQL mutation
    
    * update query using fragment
    
    * update fragments
    
    * rewrite fragments
    
    * update ChatRoomScreen index

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -10,7 +10,8 @@ import {
 } from '@testing-library/react';
 import { createBrowserHistory } from 'history';
 import { mockApolloClient } from '../../test-helpers';
-import ChatsList, { getChatsQuery } from './ChatsList';
+import ChatsList from './ChatsList';
+import * as queries from '../../graphql/queries';
 
 describe('ChatsList', () => {
   afterEach(() => {
@@ -25,7 +26,7 @@ describe('ChatsList', () => {
   it('renders fetched chats data', async () => {
     const client = mockApolloClient([
       {
-        request: { query: getChatsQuery },
+        request: { query: queries.chats },
         result: {
           data: {
             chats: [
@@ -63,7 +64,7 @@ describe('ChatsList', () => {
     it('should navigate to the target chat room on chat item click', async () => {
       const client = mockApolloClient([
         {
-          request: { query: getChatsQuery },
+          request: { query: queries.chats },
           result: {
             data: {
               chats: [
