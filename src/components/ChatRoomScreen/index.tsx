7164005b209a0889e9a2a1b64fec1d203d76c82c
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-13 23:07:57 +0700

    update cache on message added v2

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -1,20 +1,15 @@
-import { defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
 import React from 'react';
-import { useCallback } from 'react';
-// import { useQuery, useMutation } from '@apollo/react-hooks';
 import styled from 'styled-components';
+import { useCallback } from 'react';
+import { History } from 'history';
+
 import ChatNavbar from './ChatNavbar';
 import MessageInput from './MessageInput';
 import MessagesList from './MessagesList';
-import { History } from 'history';
-import * as queries from '../../graphql/queries';
+import { useGetChatQuery, useAddMessageMutation } from '../../graphql/types';
 import * as fragments from '../../graphql/fragments';
-import {
-  ChatsQuery,
-  useGetChatQuery,
-  useAddMessageMutation,
-} from '../../graphql/types';
+import { writeMessage } from '../../services/cache.service';
 
 const Container = styled.div`
   background: url(/assets/chat-background.jpg);
@@ -63,10 +58,6 @@ export interface ChatQueryResult {
 
 type OptionalChatQueryResult = ChatQueryResult | null;
 
-interface ChatsResult {
-  chats: any[];
-}
-
 const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({
   history,
   chatId,
@@ -99,74 +90,7 @@ const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({
         },
         update: (client, { data }) => {
           if (data && data.addMessage) {
-            type FullChat = { [key: string]: any };
-            let fullChat;
-            const chatIdFromStore = defaultDataIdFromObject(chat);
-
-            if (chatIdFromStore === null) {
-              return;
-            }
-
-            try {
-              fullChat = client.readFragment<FullChat>({
-                id: chatIdFromStore,
-                fragment: fragments.fullChat,
-                fragmentName: 'FullChat',
-              });
-            } catch (e) {
-              return;
-            }
-
-            if (fullChat === null || fullChat.messages === null) {
-              return;
-            }
-            if (
-              fullChat.messages.some(
-                (currentMessage: any) =>
-                  data.addMessage && currentMessage.id === data.addMessage.id
-              )
-            ) {
-              return;
-            }
-
-            fullChat.messages.push(data.addMessage);
-            fullChat.lastMessage = data.addMessage;
-
-            client.writeFragment({
-              id: chatIdFromStore,
-              fragment: fragments.fullChat,
-              fragmentName: 'FullChat',
-              data: fullChat,
-            });
-
-            let clientChatsData: ChatsQuery | null;
-            try {
-              clientChatsData = client.readQuery({
-                query: queries.chats,
-              });
-            } catch (e) {
-              return;
-            }
-
-            if (!clientChatsData || !clientChatsData.chats) {
-              return null;
-            }
-            const chats = clientChatsData.chats;
-
-            const chatIndex = chats.findIndex(
-              (currentChat: any) => currentChat.id === chatId
-            );
-            if (chatIndex === -1) return;
-            const chatWhereAdded = chats[chatIndex];
-
-            // The chat will appear at the top of the ChatsList component
-            chats.splice(chatIndex, 1);
-            chats.unshift(chatWhereAdded);
-
-            client.writeQuery({
-              query: queries.chats,
-              data: { chats: chats },
-            });
+            writeMessage(client, data.addMessage);
           }
         },
       });
