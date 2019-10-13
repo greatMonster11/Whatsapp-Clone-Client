26472e6bfd7a8736701107d8260b5eac51c8ffe1
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-13 23:17:50 +0700

    use cache service

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mockApolloClient } from './test-helpers';
+import * as subscriptions from './graphql/subscriptions';
 
 it('renders without crashing', () => {
+  const client = mockApolloClient([
+    {
+      request: { query: subscriptions.messageAdded },
+      result: { data: {} },
+    },
+  ]);
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
